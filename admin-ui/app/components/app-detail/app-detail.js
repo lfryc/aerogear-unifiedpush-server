6bde39aa91d89f95963193fec1864fa872345d3d
Lukas Fryc <lukas@fryc.eu>
2015-03-25 20:19:47 +0100

    renew master secret and send push notification dialogs

--- admin-ui/app/components/app-detail/app-detail.js
+++ admin-ui/app/components/app-detail/app-detail.js
@@ -1,5 +1,5 @@
 angular.module('upsConsole.appDetail', [])
-  .controller('AppDetailController', function($routeParams) {
+  .controller('AppDetailController', function( $routeParams, $modal ) {
 
     this.tab = $routeParams.tab;
 
@@ -7,4 +7,23 @@ angular.module('upsConsole.appDetail', [])
       id: $routeParams.app
     };
 
+    this.sendNotification = function() {
+      $modal.open({
+        templateUrl: 'views/dialogs/send-push-notification.html',
+        controller: 'SendNotificationCtrl as sendNotificationCtrl'
+      });
+    };
+
+  })
+
+  .controller('SendNotificationCtrl', function( $scope, $modalInstance ) {
+    $scope.send = function() {
+      console.log('send');
+      $modalInstance.close();
+    };
+
+    $scope.cancel = function() {
+      console.log('cancel');
+      $modalInstance.dismiss();
+    };
   });
---
+++ admin-ui/app/components/app-detail/include/sender.js
@@ -0,0 +1,19 @@
+angular.module('upsConsole.appDetail')
+  .controller('SenderController', function( $modal ) {
+    this.renewMasterSecret = function () {
+      var modal = $modal.open({
+        templateUrl: 'inline:renew-master-secret.html',
+        controller: 'DefaultModalController'
+      });
+    };
+  })
+
+  .controller('DefaultModalController', function( $scope, $modalInstance ) {
+    $scope.confirm = function () {
+      $modalInstance.close.apply($modalInstance, arguments);
+    };
+
+    $scope.dismiss = function () {
+      $modalInstance.dismiss.apply($modalInstance, arguments);
+    };
+  });
