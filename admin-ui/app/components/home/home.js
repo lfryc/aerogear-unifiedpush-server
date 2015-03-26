d626aeeea56b1bda86ba81ad3276965d458393c2
Lukas Fryc <lukas@fryc.eu>
2015-03-26 21:16:24 +0100

    loading of apps on home page

--- admin-ui/app/components/home/home.js
+++ admin-ui/app/components/home/home.js
@@ -1,10 +1,22 @@
 angular.module('upsConsole')
-  .controller('HomeController', function( $modal ) {
+  .controller('HomeController', function( $modal, applicationsEndpoint ) {
+
+    var $scope = this;
+
+    this.apps = [];
+
+    this.activate = function() {
+      applicationsEndpoint.fetch(1)
+        .then(function( result ) {
+          $scope.apps = result.page;
+        });
+    };
 
     this.deleteApp = function() {
       $modal.open({
         templateUrl: 'views/dialogs/remove-app.html',
         controller: 'DefaultModalController'
       });
-    }
+    };
+
   });
