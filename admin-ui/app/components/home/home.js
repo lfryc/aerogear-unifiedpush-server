37ce4cfc791002da7a11d98c49cbd0082eabf76c
Lukas Fryc <lukas@fryc.eu>
2015-03-27 08:58:26 +0100

    list of apps - paginated

--- admin-ui/app/components/home/home.js
+++ admin-ui/app/components/home/home.js
@@ -1,10 +1,31 @@
 angular.module('upsConsole')
-  .controller('HomeController', function( $modal ) {
+  .controller('HomeController', function( $modal, applicationsEndpoint ) {
+
+    var self = this;
+
+    this.apps = [];
+
+    this.activate = function() {
+      self.fetchNewPage(1);
+    };
+
+    this.pageChanged = function(page) {
+      self.fetchNewPage(page);
+    };
+
+    this.fetchNewPage = function(page) {
+      applicationsEndpoint.fetch(page)
+        .then(function( result ) {
+          self.apps = result.apps;
+          self.totalItems = result.totalItems;
+        });
+    };
 
     this.deleteApp = function() {
       $modal.open({
         templateUrl: 'views/dialogs/remove-app.html',
         controller: 'DefaultModalController'
       });
-    }
+    };
+
   });
