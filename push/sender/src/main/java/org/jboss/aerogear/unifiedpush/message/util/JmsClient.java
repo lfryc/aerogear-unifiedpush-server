5c509bbf58e68c8ad72dc128deec0d549601f389
Lukas Fryc <lukas@fryc.eu>
2016-01-29 13:11:10 +0100

    configuration specific for wildfly 10 / activemq artemis

--- push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/util/JmsClient.java
+++ push/sender/src/main/java/org/jboss/aerogear/unifiedpush/message/util/JmsClient.java
@@ -238,6 +238,7 @@ public class JmsClient {
                 throw new NullPointerException("duplicateDetectionId");
             }
             this.properties.put("_HQ_DUPL_ID", duplicateDetectionId);
+            this.properties.put("_AMQ_DUPL_ID", duplicateDetectionId);
             return this;
         }
 
@@ -249,6 +250,7 @@ public class JmsClient {
                 throw new NullPointerException("delayMs");
             }
             this.properties.put("_HQ_SCHED_DELIVERY", new Long(System.currentTimeMillis() + delayMs));
+            this.properties.put("_AMQ_SCHED_DELIVERY", new Long(System.currentTimeMillis() + delayMs));
             return this;
         }
 
